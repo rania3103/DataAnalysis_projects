3c2d994173305b0af3f27b62a1e2a90c7d9e0cdf
rania3103 <raniaabassi33@gmail.com>
2024-05-08 09:59:02 +0100

    add files

---
+++ Demographic Data Analyzer/main.py
@@ -0,0 +1,5 @@
+# This entrypoint file to be used in development.
+import demographic_data_analyzer
+
+# Test your function by calling it here
+demographic_data_analyzer.calculate_demographic_data()
