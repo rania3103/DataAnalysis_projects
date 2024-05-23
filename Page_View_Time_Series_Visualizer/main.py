639b81e1f9f1a2f87d5cb8149313bf177592904b
rania3103 <raniaabassi33@gmail.com>
2024-05-23 11:57:50 +0100

    time series visualization

---
+++ Page_View_Time_Series_Visualizer/main.py
@@ -0,0 +1,8 @@
+# This entrypoint file to be used in development.
+import time_series_visualizer
+from unittest import main
+
+# Test your function by calling it here
+time_series_visualizer.draw_line_plot()
+time_series_visualizer.draw_bar_plot()
+time_series_visualizer.draw_box_plot()
